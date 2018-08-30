31b8d5dd054b3c43b1d2eaad1754f2d776a74899
oturgut <ozhanturgut@hotmail.com>
2018-08-30 13:55:43 -0700

    A simple decimal adder.

---
+++ decimal_calculator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	double first_number;
+	double second_number;
+	cout << "Enter the first number: \n";
+	cin >>  first_number;
+	cout << "Enter the second number: \n";
+	cin >>  second_number;
+	cout<< "The result is: " << first_number + second_number << endl;
+	return 0;
+}
