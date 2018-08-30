87508201a84330f8d77e0a300aa3bd461eecf8c5
ozhan_github <ozhanturgut@hotmail.com>
2018-08-30 14:00:17 -0700

    Added multiplication, addition, and subtraction.

--- decimal_calculator.cpp
+++ decimal_calculator.cpp
@@ -8,6 +8,9 @@ int main() {
 	cin >>  first_number;
 	cout << "Enter the second number: \n";
 	cin >>  second_number;
-	cout<< "The result is: " << first_number + second_number << endl;
+	cout << "The sum is: " << first_number + second_number << endl;
+	cout << "The multiplication is: " << first_number * second_number << endl;
+	cout << "The division is: " << first_number / second_number << endl;
+	cout << "The substraction is: " << first_number - second_number << endl;
 	return 0;
 }
